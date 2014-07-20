e0de32220f45f43cbc5c384a00e0617307c381f9
Frank von Eitzen <fveitzen@gmail.com>
2014-07-20 14:52:25 +0200

    Initial commit for basic UI. Still with autolayout.

---
+++ myScore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myScore
+//
+//  Created by Frank on 20.07.14.
+//  Copyright (c) 2014 Frank von Eitzen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
